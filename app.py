49b135c927591710f4cbbac3949ec961fdd7e0db
jagadeeshballajb <ballajagadeesh99@gmail.com>
2026-02-08 22:47:52 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+# Dummy product data (normally DB lo untadi)
+products = [
+    {"name": "Mobile Phone", "price": "₹15,000"},
+    {"name": "Laptop", "price": "₹55,000"},
+    {"name": "Headphones", "price": "₹2,000"}
+]
+
+@app.route("/")
+def home():
+    return render_template("index.html", products=products)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
