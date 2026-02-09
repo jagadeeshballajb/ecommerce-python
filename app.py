cb0b154db158e083f98c55205b98b9714283f823
jagadeeshballajb <ballajagadeesh99@gmail.com>
2026-02-09 20:36:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 # Dummy product data (normally DB lo untadi)
 products = [
-    {"name": "Mobile Phone", "price": "₹15,000"},
+    {"name": "Mobile Phone", "price": "₹1hdgh9378340"},
     {"name": "Laptop", "price": "₹111,000"},
     {"name": "Headphones", "price": "₹2,000"}
 ]
